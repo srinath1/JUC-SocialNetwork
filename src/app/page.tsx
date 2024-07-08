c3e1d77256dbdc4fb529a5ec75f7d4db06d668fd
Srinath N.S <srinath.erp@gmail.com>
2024-07-08 15:34:56 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Timeline from "./(private)/_components/Timeline";
 import TimeLineHeader from "./(private)/_components/TimeLineHeader";
+import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <div className="grid lg:grid-cols-4 mb-5">
       <div className="col-span-2">
-        <TimeLineHeader />
-        <Timeline />
+        <Suspense fallback={<>Loading...</>}>
+          <TimeLineHeader />
+          <Timeline />
+        </Suspense>
       </div>
     </div>
   );
